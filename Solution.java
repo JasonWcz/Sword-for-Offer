ca60f85648f6aea6a2ce3bb576c8ee1fcd621e1a
jason_wu <wcz_jason@qq.com>
2019-09-25 11:19:01 +0800

    面试题3-1

---
+++ Solution.java
@@ -0,0 +1,31 @@
+package SwordForOffer;
+
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(length <= 0 || numbers.length == 0) return false;
+        for(int i = 0; i < length; i++){
+            if(numbers[i] < 0 || numbers[i] > length - 1) return false;
+        }
+        for(int i = 0; i < length; i++){
+            while(numbers[i] != i){
+                if(numbers[i] == numbers[numbers[i]]){
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                int tmp;
+                tmp = numbers[i];
+                numbers[i] = numbers[tmp];
+                numbers[tmp] = tmp;
+            }
+        }
+        return false;
+    }
+}
