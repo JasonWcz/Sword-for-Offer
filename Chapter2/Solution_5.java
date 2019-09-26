643a39de74f9b0e7326a60ee3d41391f940fd231
jason_wu <wcz_jason@qq.com>
2019-09-26 20:47:46 +0800

    Problem 5

---
+++ Chapter2/Solution_5.java
@@ -0,0 +1,32 @@
+package SwordForOffer.Chapter2;
+
+public class Solution_5 {
+    public String replaceSpace(StringBuffer str) {
+        if(str == null || str.length() <= 0) return "";
+        int len = str.length(), blank = 0;
+        for(int i = 0; i < len; i++){
+            if(str.charAt(i) == ' ') ++blank;
+        }
+        str.setLength(len + blank * 2);
+        int p1, p2;
+        p1 = len - 1; p2 = len + blank * 2 - 1;
+        while(p2 >= 0){
+            if(str.charAt(p1) != ' '){
+                str.setCharAt(p2, str.charAt(p1));
+                --p2;
+            }
+            else{
+                str.setCharAt(p2, '0');
+                str.setCharAt(p2 - 1, '2');
+                str.setCharAt(p2 - 2, '%');
+                p2 -= 3;
+            }
+            --p1;
+        }
+        return new String(str);
+    }
+    public static void main(String[] args) {
+        Solution_5 sl = new Solution_5();
+        System.out.println(sl.replaceSpace(new StringBuffer("We Are Happy")));
+    }
+}
