2d100e1c1f1532585ad44eb0b422f82f7e3d2957
jason_wu <wcz_jason@qq.com>
2019-09-26 21:11:36 +0800

    Problem 6 从尾到头打印链表

---
+++ Chapter2/Solution_6.java
@@ -0,0 +1,36 @@
+package SwordForOffer.Chapter2;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution_6 {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        //递归写法
+        if(listNode == null) return new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(this.printListFromTailToHead(listNode.next)); //此处注意addAll添加在前面
+        list.add(listNode.val);
+        return list;
+
+        //栈写法
+//        Stack<Integer> stack = new Stack<>();
+//        while(listNode != null){
+//            stack.add(listNode.val);
+//            listNode = listNode.next;
+//        }
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while(!stack.empty()){
+//            list.add(stack.pop());
+//        }
+//        return list;
+    }
+}
