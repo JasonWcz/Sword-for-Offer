45dd30e3b5b007ed68cddda81cab7cd9e75f0785
jason_wu <wcz_jason@qq.com>
2019-09-27 15:30:26 +0800

    Problem 7 前序中序重建二叉树

---
+++ Chapter2/Solution_7.java
@@ -0,0 +1,45 @@
+package SwordForOffer.Chapter2;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+public class Solution_7 {
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        try {
+            if (pre == null || pre.length <= 0) return null;
+            int len = pre.length;
+            return this.Construct(pre, in, 0, len - 1, 0, len - 1);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public TreeNode Construct(int[] pre, int[] in, int preStart, int preEnd, int inStart, int inEnd) throws Exception {
+        int rootVal = pre[preStart];
+        TreeNode root = new TreeNode(rootVal);
+        root.left = root.right = null;
+        if (preStart == preEnd) {
+            if (inStart == inEnd && pre[preStart] == in[inStart]) return root; //叶子
+            else throw new Exception("Invalid Input");
+        }
+        int rootPos = inStart;
+        while (rootPos <= inEnd && in[rootPos] != rootVal) ++rootPos; //找到root在中序里的位置
+        if (rootPos == inEnd && in[rootPos] != rootVal) throw new Exception("Invalid Input");
+        int leftLen = rootPos - inStart;
+        int leftPreEnd = preStart + leftLen;
+        if (leftLen > 0) {
+            root.left = Construct(pre, in, preStart + 1, leftPreEnd, inStart, rootPos - 1);
+        }
+        if (leftLen < preEnd - preStart) {
+            root.right = Construct(pre, in, leftPreEnd + 1, preEnd, rootPos + 1, inEnd);
+        }
+        return root;
+    }
+    
+}
