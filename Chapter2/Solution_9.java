86bd25388e8954d1d8d8de1582c3fc2b83a04fe0
jason_wu <wcz_jason@qq.com>
2019-09-29 13:41:34 +0800

    Problem 9 两个栈模拟队列

---
+++ Chapter2/Solution_9.java
@@ -0,0 +1,26 @@
+package SwordForOffer.Chapter2;
+
+import java.util.Stack;
+
+public class Solution_9 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if(stack2.empty()) try {
+            throw new Exception("no node");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stack2.pop();
+    }
+}
