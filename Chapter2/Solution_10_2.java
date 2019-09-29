8784719db93a3a1f6a170987da053cb8b4e53f61
jason_wu <wcz_jason@qq.com>
2019-09-29 13:55:45 +0800

    Problem 10_2 跳台阶(Fibonacci应用)

--- Chapter2/Solution_10_2.java
+++ Chapter2/Solution_10_2.java
@@ -1,4 +1,15 @@
 package SwordForOffer.Chapter2;
 
 public class Solution_10_2 {
+    public int JumpFloor(int target){
+        if(target == 0) return 1;
+        if(target == 1) return 1;
+        int first = 1, second = 1, tmp = 0;
+        for(int i = 2; i <= target; i++){
+            tmp = first + second;
+            first = second;
+            second = tmp;
+        }
+        return tmp;
+    }
 }
