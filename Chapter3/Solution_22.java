82c8046bec3a25082ec26205c915341345ab06c1
JasonWcz <wcz_jason@qq.com>
2019-12-15 19:49:12 +0800

    Update Solution_22.java

--- Chapter3/Solution_22.java
+++ Chapter3/Solution_22.java
@@ -34,14 +34,4 @@ public class Solution_22 {
         }
         return pNode;
     }
-
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        ListNode res = FindKthToTail(head,3);
-        System.out.println(res.val);
-    }
 }
