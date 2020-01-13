2a777944c67a930018a12fea36d0860c44a37d52
jason_wu <wcz_jason@qq.com>
2020-01-13 12:01:17 +0800

    Problem 55_1 二叉树的深度

---
+++ Chapter6/Solution_55_1.java
@@ -0,0 +1,23 @@
+package SwordForOffer.Chapter6;
+
+public class Solution_55_1 {
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public int TreeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return (left > right) ? left + 1 : right + 1;
+    }
+}
--- README.md
+++ README.md
@@ -66,4 +66,5 @@
     - [Problem 53_2 0到n-1中缺失的数字](https://github.com/JasonWcz/Sword-for-Offer/blob/master/Chapter6/Solution_53_2.java)
     - [Problem 53_3 数组中数值和下标相等的元素](https://github.com/JasonWcz/Sword-for-Offer/blob/master/Chapter6/Solution_53_3.java)
     - [Problem 54 二叉搜索树的第K大节点](https://github.com/JasonWcz/Sword-for-Offer/blob/master/Chapter6/Solution_54.java)
+    - [Problem 55_1 二叉树的深度](https://github.com/JasonWcz/Sword-for-Offer/blob/master/Chapter6/Solution_55_1.java)
     
